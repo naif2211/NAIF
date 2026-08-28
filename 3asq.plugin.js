49c8847cb86e28a4e519abb6622eb90c2424c8e0
naif2211 <naif.com070@gmail.com>
2026-08-28 11:03:08 +0300

    Restore 3asq plugin as provided by user

--- 3asq.plugin.js
+++ 3asq.plugin.js
@@ -174,7 +174,6 @@ async function chapterList(id) {
   let list = chaptersFromDoc(doc);
   if (list.length) return list;
 
-  // Madara fallback: chapters may be loaded by AJAX.
   const pid = postId(doc);
   if (pid) {
     try {
